3f08b0cdfe1bdfbcff8e7a13e17643a99f408913
vaibhavchellani <vaibhavchellani223@gmail.com>
2017-05-19 13:11:56 +0530

    added listview and connected to adapters , created Message class , username to be taken from signin

--- app/src/main/java/com/example/vaibhavchellani/firebaseioextended/MainActivity.java
+++ app/src/main/java/com/example/vaibhavchellani/firebaseioextended/MainActivity.java
@@ -2,12 +2,59 @@ package com.example.vaibhavchellani.firebaseioextended;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText messageEditText;
+    private Button sendButton;
+    private ListView messageListView;
+    private listviewAdapter mlistviewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signin_layout);
+        setContentView(R.layout.activity_main);
+
+        //linking xml and java
+        messageEditText=(EditText) findViewById(R.id.messageEditText);
+        sendButton=(Button) findViewById(R.id.sendButton);
+        messageListView=(ListView)findViewById(R.id.messageListView);
+        List<Message> messages=new ArrayList<Message>();
+        Message newMessage=new Message();
+
+        //for testing listview
+        /*newMessage.setUserMessage("fdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfd");
+        newMessage.setUserName("vaibhav");
+        messages.add(newMessage);
+        messages.add(newMessage);messages.add(newMessage);messages.add(newMessage);*/
+
+
+        mlistviewAdapter=new listviewAdapter(this,R.layout.row_layout,messages);
+        messageListView.setAdapter(mlistviewAdapter);
+
+        // Send button listener
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
     }
 }
